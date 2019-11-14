2d4d1e5e2c07fe47487ee4ea69b1b300b21c4793
9kongpob <k.kunchanuan@hotmail.com>
2019-11-14 15:13:47 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasicPart02_InputOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Input your birthyear: ");
+            int result;
+            int.TryParse(Console.ReadLine(), out result);
+            result = result + 543;
+            Console.WriteLine("My name is : " + name);
+            Console.WriteLine("My birthday is " + result);
+           
+            Console.ReadKey();
+        }
+    }
+}
